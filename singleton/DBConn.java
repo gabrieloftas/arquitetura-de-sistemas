ec572e3cf6243967eda66ed2219700f975086e03
gabrieloftas <gabrieloftas@outlook.com>
2025-09-25 11:02:29 -0300

    feat:implementado singleton para dbconn

---
+++ singleton/DBConn.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class DBConn {
+    private static DBConn instance;
+
+    private DBConn(){}
+
+    public static DBConn getInstance(){
+        if(instance == null){
+            instance = new DBConn();
+        }
+        return instance;
+    }
+
+    public void connect(){
+        System.out.println("DB Connected");
+    }
+}
